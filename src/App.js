e4e14b02fc75933a941b856bab98f0495da1a720
leonardo-da <cruz.leonardodaniel.284ags@gmail.com>
2022-11-08 23:47:57 -0600

    images home page, design home page

--- src/App.js
+++ src/App.js
@@ -1,63 +1,52 @@
 import './App.css';
-import {Nav,NavDropdown,Navbar,Container,Table} from "react-bootstrap"
+import {Nav,Navbar,Container,Carousel} from "react-bootstrap"
 
+import ensamble from "./assets/img/ensamble.jpg";
+import redes from "./assets/img/redes.jpg";
+import impresora from "./assets/img/impresora.jpg";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="white" expand="md">
     <Container>
       <Navbar.Brand href="#home">SIA</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
+          <Nav.Link href="#home">Servicios</Nav.Link>
+          <Nav.Link href="#link">Productos</Nav.Link>
+          <Nav.Link href="#link">Soporte</Nav.Link>
+          <Nav.Link href="#link">Seguridad</Nav.Link>
+          <Nav.Link href="#link">Contacto</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
   </Navbar>
   
-  <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
+  <Carousel fade className="w-75 position-absolute top-50 start-50 translate-middle">
+      <Carousel.Item>
+        <img className="w-100 col" src={ensamble} alt="First slide"/>
+        <Carousel.Caption>
+          <h3 >PC</h3>
+          <p>Ensamble, reparación de equipos de computo.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="w-100" src={redes} alt="Second slide"/>
+        <Carousel.Caption>
+          <h3>REDES</h3>
+          <p>Diseño, instalación y configuracion de redes informaticas.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="w-100" src={impresora} alt="Third slide"/>
+        <Carousel.Caption>
+          <h3>IMPRESORAS</h3>
+          <p>Mantenimiento, reparación y consumibles para su impresora.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
     </div>
   );
 }
