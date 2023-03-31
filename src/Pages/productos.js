aaf1575b683182e9df715d1c3674191a8e800754
leonardo-da <cruz.leonardodaniel.284ags@gmail.com>
2023-03-30 23:30:33 -0600

    product page

--- src/Pages/productos.js
+++ src/Pages/productos.js
@@ -1,19 +1,50 @@
-import { Card,Col,Row,Container} from "react-bootstrap";
+import { Table} from "react-bootstrap";
 
 import PC from '../assets/img/PC.png';
+import PRO from '../assets/img/procesador.png';
+import FAN from '../assets/img/ventilador.png';
+import RAM from '../assets/img/ram.png';
+import GAB from '../assets/img/Gabinete.png';
+import GRA from '../assets/img/grafica.png';
+import POD from '../assets/img/poder.png';
+import MAD from '../assets/img/madre.png';
+import ALM from '../assets/img/SSD.png';
+import PER from '../assets/img/perisfericos.png';
+
 
 const App = () =>{
 
     return(
       <>
       <div style={{background:'#636363',marginTop:'6rem'}} className="text-white">
-        <h2 className="text-center pt-3">Ensamblamos su PC de acuerdo a sus necesidades.</h2>
+        <h2 className="text-center pt-5">Ensamblamos su PC de acuerdo a sus necesidades.</h2>
         <h3 className="text-center">Configurado a su estilo de trabajo</h3>
-        <h4 className="text-center mt-5">*Los mejores componentes</h4>
-        <img className="w-50 mx-auto d-block" src={PC}></img>
 
+        <div class="mt-5 d-flex">
+        <img className="w-50 mx-5" src={PC}></img>
+        <Table className="w-50 mx-5 text-center text-white" bordered>
+          <tbody>
+            <tr>
+              <td><div>Procesador</div><img className="w-50" src={PRO}></img></td>
+              <td><div>Memoria</div><img className="w-50" src={RAM}></img></td>
+              <td><div>Disipación</div><img className="w-50" src={FAN}></img></td>
+            </tr>
+            <tr> 
+            <td><div>Gabinete</div><img className="w-50" src={GAB}></img></td>
+            <td><div>Tarjeta Grafica</div><img className="w-50" src={GRA}></img></td>
+            <td><div>Fuente de poder</div><img className="w-50" src={POD}></img></td>
+            </tr>
+            <tr>
+            <td><div>Procesador</div><img className="w-50" src={MAD}></img></td>
+            <td><div>Almacenamiento</div><img className="w-50" src={ALM}></img></td>
+            <td><div>Perisfericos</div><img className="w-50" src={PER}></img></td>
+            </tr>
+          </tbody>
+        </Table>
 
       </div>
+      </div>
+      
       </>
  
 
--- src/Pages/servicios.js
+++ src/Pages/servicios.js
@@ -1,53 +1,9 @@
 import { Card,Col,Row,Container} from "react-bootstrap";
-import IMG0 from '../assets/img/preventivo.png';
-import IMG1 from '../assets/img/correctivo.png';
-import IMG2 from '../assets/img/servidor.png';
-import IMG3 from '../assets/img/redes.png';
 
 const App = () =>{
-  const tables = [
-    {nameI:`${IMG0}`,title:"Mantenimiento preventivo",contenent:"Notaste que tu equipo está más lento o que está más caliente de lo normal, un servicio preventivo podría solucionar el problema para evitar problemas futuros en tu equipo."},
-    {nameI:`${IMG1}`,title:"Mantenimiento correctivo",contenent:"Tu equipo ha sufrido algún daño, o simplemente dejo de encender, al realizar un servicio correctivo podremos determinar la falla del equipo y la solución para revivir tu equipo y que funcione de forma correcta."},
-  ]
-  const tables1 = [
-    {nameI:`${IMG2}`,title:"Servidores",contenent:"Un servidor para tu empresa es más que necesario, adaptándolo a las necesidades de la empresa tanto para las tareas por realizar y la información que resguardar realizando las configuraciones adecuadas en el sistema operátivo y en la red."},
-    {nameI:`${IMG3}`,title:"Redes",contenent:"Configuración de Pc's y Laptops en red, instalación de impresoras y plotters para el fácil acceso de los usuarios en red. Soporte tecnico, reparación, modificación, adecuación y configuración de redes alámbricas (LAN) e inalámbricas (WLAN). Configuración e instalación de cableado estructurado, punto a punto y sistemas en red."}
-  ]
     return(
       <Container>
-        <div data-aos="fade-right" style={{marginTop:'9rem'}}>
-          <h1 className="border-bottom">Nuestros Servicios</h1>
-          <Row className=" row-cols-1 row-cols-md-2 g-4">
-            {tables.map((table) => {
-              return (
-                <Col>
-                  <Card className="p-4 m-4 align-items-center border-0">
-                    <Card.Img variant="top" src={table.nameI} className="w-75"/>
-                    <Card.Body>
-                      <Card.Title>{table.title}</Card.Title>
-                      <Card.Text>{table.contenent}</Card.Text>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              )})}
-          </Row>
-        </div>
-        <div data-aos="fade-left">
-          <Row className=" row-cols-1 row-cols-md-2 g-4">
-            {tables1.map((table) => {
-              return (
-                <Col>
-                  <Card className="p-4 m-4 align-items-center border-0">
-                    <Card.Img variant="top" src={table.nameI} className="w-75"/>
-                    <Card.Body>
-                      <Card.Title>{table.title}</Card.Title>
-                      <Card.Text>{table.contenent}</Card.Text>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              )})}
-          </Row>
-        </div>
+        
       </Container>
     );
 }
